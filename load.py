ea153e1148d4dd387db01f06cc665b57c7fe2e88
iConfesss <e.kamerbeek@student.fontys.nl>
2016-11-03 14:44:21 +0100

    Partial load added

--- load.py
+++ load.py
@@ -7,4 +7,14 @@
 # key value where key is studentnumber and value is rest
 
 import csv
+with open('input_files/studenten_roostering_1.csv', 'r') as csvfile:
+    try:
+        student = csv.reader(csvfile)
+        break
+    except LookupError:
+        print ""
+    except:
+        print "Can not open the selected file"
 
+    for row in student:
+        # store info per row
