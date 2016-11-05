45b9d0bce8e368c5d0e8a917c2c70214810ef068
iConfesss <e.kamerbeek@student.fontys.nl>
2016-11-05 13:16:37 +0100

    Updates on issues #2 and #3 Removed unnecessary getters Use of args instead of hardcoding input

--- classes.py
+++ classes.py
@@ -7,24 +7,11 @@ class Student(object):
     # How to:
     # store the class (array? or self.class1 = x, self.class2 = null etc
 
-    def __init__(self, id, last_name, first_name, subject1 = None, subject2 = None, subject3 = None, subject4 = None, subject5 = None):
+    def __init__(self, id, last_name, first_name, *args):
         self.id = id
         self.last_name = last_name
         self.first_name = first_name
-        self.subjects = [subject1, subject2, subject3, subject4, subject5]
-
-    def get_id(self):
-        return self.id
-
-    def get_last_name(self):
-        return self.last_name
-
-    def get_first_name(self):
-        return self.first_name
-
-    def get_subjects(self):
-        return self.subjects
-
+        self.subjects = args
 
 """
 
@@ -35,13 +22,6 @@ class Room(object):
     def __init__(self, name, capacity):
         self.name = name
         self.capacity = capacity
-
-    def get_name(self):
-        return self.name
-
-    def get_capacity(self):
-        return self.capacity
-
 
 """
 
@@ -60,21 +40,3 @@ class Subject(object):
         self.seminar_max_students = seminar_max_students
         self.q_practicum = q_practicum
         self.practicum_max_students = practicum_max_students
-
-    def get_name(self):
-        return self.name
-
-    def get_q_lecture(self):
-        return self.q_lecture
-
-    def get_q_seminar(self):
-        return self.q_seminar
-
-    def get_seminar_max_students(self):
-        return self.seminar_max_students
-
-    def get_q_practicum(self):
-        return self.q_practicum
-
-    def get_practicum_max_students(self):
-        return self.practicum_max_students
