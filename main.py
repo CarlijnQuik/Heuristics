dda90746b2c2090a3c70636f9a5cbeda53a68afe
iConfesss <e.kamerbeek@student.fontys.nl>
2016-11-10 17:03:05 +0100

    Revert "Merge pull request #4 from carlijnq/Chris"
    This reverts commit d3889bc08979c81f138aa9aa5111ede6c11b506a, reversing
    changes made to 14e2efa82dc89e7542758d38f63997fe82ed806d.

--- main.py
+++ main.py
@@ -4,34 +4,24 @@
 import load as loada
 import ptp, visual
 
-debug = False
-if debug or raw_input('Enter def for default input or enter to continue manually: ') == 'def':
-    students = loada.load_students('studenten_roostering.csv')
-    subjects = loada.load_subjects('vakken.csv')
-    rooms = loada.load_rooms('zalen.csv')
+student_file = raw_input('Student input file: ')
+while not student_file:
+    student_file = raw_input('Student input file(CSV Format): ')
 
-else:
-    student_file = raw_input('Student input file: ')
-    while not student_file:
-        student_file = raw_input('Student input file(CSV Format): ')
+students = loada.load_students(student_file)
 
-    students = loada.load_students(student_file)
+subject_file = raw_input('Subject input file: ')
+while not subject_file:
+    subject_file = raw_input('Subject input file(CSV Format): ')
 
-    subject_file = raw_input('Subject input file: ')
-    while not subject_file:
-        subject_file = raw_input('Subject input file(CSV Format): ')
+subjects = loada.load_subjects(subject_file)
 
-    subjects = loada.load_subjects(subject_file)
+room_file = raw_input('Room input file: ')
+while not room_file:
+    room_file = raw_input('Room input file(CSV Format): ')
 
-    room_file = raw_input('Room input file: ')
-    while not room_file:
-        room_file = raw_input('Room input file(CSV Format): ')
-
-    rooms = loada.load_rooms(room_file)
+rooms = loada.load_rooms(room_file)
 
 loada.load_students_to_subjects(students, subjects)
-print '\n\n'
-print 'DONE LOADING!'
-
-# create dictionaries to represent a timetable of a week
-# note that this timetable is hardcoded. More softcoding would be prefered
+print '\n'
+print ' DONE LOADING!'
