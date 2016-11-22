e0b08024a63671986d99fadeea57005b675fa80e
Levyo <Chris_de_Rijcke@hotmail.com>
2016-11-22 12:24:24 +0100

    basic hillclimber algorithm

--- ptp.py
+++ ptp.py
@@ -1,5 +1,7 @@
 import schedule_class
 import score
+import random
+import time
 
 TYPE_LECTURE = 'lecture'
 TYPE_SEMINAR = 'seminar'
@@ -21,9 +23,50 @@ def alg(students, courses, rooms, schedule):
 
     # Get score for the Schedule
     score.calculate(schedule, courses)
+    return schedule
 
 # TODO: Add find empty for specific day/room/time (?)
 def find_empty(schedule, max_size = 0):
     for i, roomslot in enumerate(schedule):
         if roomslot.course is None and roomslot.type is None:
             return i
+
+def basic_hillclimber(schedule, courses, desired_score):
+    # to keep track of running time
+    start_time = time.time()
+    # calculate starting score of schedule
+    score_schedule = score.calculate(schedule, courses)
+    new_schedule = schedule
+    #for i in range(10):
+    while score_schedule < desired_score:
+        print "new round, new chances"
+        old_score = score.calculate(new_schedule, courses)
+
+        # create random swap indexes
+        swap_index_1 = random.randrange(len(new_schedule))
+        swap_index_2 = random.randrange(len(new_schedule))
+
+        # swap courses and course types
+        temp_course = new_schedule[swap_index_1].course
+        temp_type = new_schedule[swap_index_1].type
+        new_schedule[swap_index_1].course = new_schedule[swap_index_2].course
+        new_schedule[swap_index_1].type = new_schedule[swap_index_2].type
+        new_schedule[swap_index_2].course = temp_course
+        new_schedule[swap_index_2].type = temp_type
+        new_score = score.calculate(new_schedule, courses)
+        if new_score < old_score:
+            # swap the courses back to their original place
+            temp_course = new_schedule[swap_index_1].course
+            temp_type = new_schedule[swap_index_1].type
+            new_schedule[swap_index_1].course = new_schedule[swap_index_2].course
+            new_schedule[swap_index_1].type = new_schedule[swap_index_2].type
+            new_schedule[swap_index_2].course = temp_course
+            new_schedule[swap_index_2].type = temp_type
+            print "\tschedules stays the same"
+        else:
+            score_schedule = new_score
+        print "\t score in between: ", score_schedule, "\n"
+    #calculate running time
+    elapsed_time = time.time() - start_time
+    print "\tbasic_hillclimber; score: ", score_schedule, "elapsed time: ", elapsed_time
+    return {"schedule" : schedule, "score" : score_schedule, "elapsed_time" : elapsed_time}
