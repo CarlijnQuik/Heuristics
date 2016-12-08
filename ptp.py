303472497ddedd5f450cb3431904180ed6f88e48
Levyo <Chris_de_Rijcke@hotmail.com>
2016-12-08 22:37:18 +0100

    random_hillclimber is still in ptp
    also a plot has been added

--- ptp.py
+++ ptp.py
@@ -4,6 +4,8 @@ import random
 import time
 import activity
 import copy
+import matplotlib.pyplot as plt
+import pylab
 from collections import deque
 
 def alg(students, courses, rooms, schedule):
@@ -12,11 +14,19 @@ def alg(students, courses, rooms, schedule):
     return schedule
 
 # TODO: Add find empty for specific day/room/time (?)
+# Does find empty work probably? None is returned when a schedule in a algorithm is placed.
 def find_empty(schedule, max_size = 0):
     for i, roomslot in enumerate(schedule):
         if roomslot.activity is None:
             return i
 
+# maybe a store function that it remembers what has already been added if the same schedule is checked again
+def find_empty_random(schedule, max_size = 0):
+    empty_room_list = []
+    for i, roomslot in enumerate(schedule):
+        if roomslot.activity.course is None:
+            empty_room_list.append(i)
+    return random.choice(empty_room_list)
 
 
 #
@@ -32,7 +42,8 @@ def random_hillclimber(schedule, courses, desired_score):
     update_times = []
     update_times.append(start_time - start_time)
 
-    while score_schedule < desired_score:
+    for i in range(1):
+    #while score_schedule < desired_score:
         new_schedule = copy.deepcopy(schedule)
         old_score = score.calculate(new_schedule, courses)
 
@@ -56,129 +67,20 @@ def random_hillclimber(schedule, courses, desired_score):
         if new_score > old_score:
             schedule = copy.deepcopy(new_schedule)
             score_schedule = new_score
-
-        update_time = time.time() - start_time
-        if int(update_time / 60) != int(update_times[-1] / 60):
-            update_times.append(update_time)
-            score_increase.append(score_schedule)
-            if (score_increase[-1] - score_increase[-2]) < 15:
-                break
-    elapsed_time = time.time() - start_time
-    print "\trandom_hillclimber; score: ", score_schedule, "elapsed time: ", int(elapsed_time / 60),"Min", int(elapsed_time % 60), "sec"
-    print update_times
-    print score_increase
-    return {"schedule" : schedule, "score" : score_schedule, "elapsed_time" : elapsed_time}
-
-
-# TODO has to be written to a real simulated annealer.
-def random_simulated_annealer(schedule, courses, desired_score):
-    start_time = time.time()
-    # calculate starting score of schedule
-    score_schedule = score.calculate(schedule, courses)
-    new_schedule = copy.copy(schedule)
-    score_increase = []
-    score_increase.append(score_schedule)
-    update_times = []
-    update_times.append(start_time - start_time)
-
-    while score_schedule < desired_score:
-        new_schedule = copy.deepcopy(schedule)
-        old_score = score.calculate(new_schedule, courses)
-
-        # create random swap indexes
-        swap_index_1 = random.randrange(len(schedule))
-        swap_index_2 = random.randrange(len(schedule))
-
-        for i in range(1):
-            # create random swap indexes
-            swap_index_1 = random.randrange(len(new_schedule))
-            swap_index_2 = random.randrange(len(new_schedule))
-            # swap courses and course types
-            temp_course = new_schedule[swap_index_1].activity.course
-            temp_type = new_schedule[swap_index_1].activity.type
-            new_schedule[swap_index_1].activity.course = new_schedule[swap_index_2].activity.course
-            new_schedule[swap_index_1].activity.type = new_schedule[swap_index_2].activity.type
-            new_schedule[swap_index_2].activity.course = temp_course
-            new_schedule[swap_index_2].activity.type = temp_type
-
-        new_score = score.calculate(new_schedule, courses)
-        if new_score > old_score:
-            schedule = copy.deepcopy(new_schedule)
-            score_schedule = new_score
-
-
 
         update_time = time.time() - start_time
         if int(update_time) != int(update_times[-1]):
             update_times.append(update_time)
-            print update_times
             score_increase.append(score_schedule)
-            print len(score_increase)
             if len(score_increase) >= 61:
-                if (score_increase[-1] - score_increase[-61]) < 1500:
+                if (score_increase[-1] - score_increase[-61]) < 15:
                     break
     elapsed_time = time.time() - start_time
-    print score_increase
-    print "\trandom_simulated_annealer; score: ", score_schedule, "elapsed time: ", int(elapsed_time / 60),"Min", int(elapsed_time % 60), "sec"
-    return {"schedule" : schedule, "score" : score_schedule, "elapsed_time" : elapsed_time}
-
-# still in process 
-def guided_hillclimber(schedule, courses, desired_score):
-    #check_special_timeslot(schedule)
-    #check_spreading(schedule, courses)
-    #check_day_duplicate(schedule)
-    #check_multiple_activities(schedule)
-    #check_small_room(schedule)
-
-    start_time = time.time()
-    # calculate starting score of schedule
-    score_schedule = score.calculate(schedule, courses)
-    new_schedule = copy.copy(schedule)
-    score_increase = []
-    score_increase.append(score_schedule)
-    update_times = []
-    update_times.append(start_time - start_time)
-
-    while score_schedule < desired_score:
-        new_schedule = copy.deepcopy(schedule)
-        old_score = score.calculate(new_schedule, courses)
-        STS = score.check_special_timeslot(new_schedule)
-        print STS
-        if STS[1] is not 0:
-            swap_index_1 = STS[0][0]
-            print STS[0][0]
-            print new_schedule[STS[0][0]].activity.course.seminar_max_students
-            empty_place = find_empty(new_schedule)
-            print empty_place
-            swap_index_2 = empty_place
-            #swap_index_2 = random.randrange(len(schedule))
-        else:
-            # create random swap indexes
-            swap_index_1 = random.randrange(len(schedule))
-            swap_index_2 = random.randrange(len(schedule))
-
-
-        # create random swap indexes
-        # swap courses and course types
-        temp_course = new_schedule[swap_index_1].activity.course
-        temp_type = new_schedule[swap_index_1].activity.type
-        new_schedule[swap_index_1].activity.course = new_schedule[swap_index_2].activity.course
-        new_schedule[swap_index_1].activity.type = new_schedule[swap_index_2].activity.type
-        new_schedule[swap_index_2].activity.course = temp_course
-        new_schedule[swap_index_2].activity.type = temp_type
-
-        new_score = score.calculate(new_schedule, courses)
-        if new_score > old_score:
-            schedule = copy.deepcopy(new_schedule)
-            score_schedule = new_score
-
-        update_time = time.time() - start_time
-        if int(update_time / 60) != int(update_times[-1] / 60):
-            update_times.append(update_time)
-            score_increase.append(score_schedule)
-            if (score_increase[-1] - score_increase[-2]) < 15:
-                break
-    elapsed_time = time.time() - start_time
+    plt.plot(update_times, score_increase)
+    print "random_hillclimber_"+str(desired_score)+"_"+str(int(elapsed_time))
+    plt.title("random_hillclimber; desired_score: " +str(desired_score) + ", elapsed_time: "+ str(int(elapsed_time)))
+    plt.savefig("random_hillclimber; desired_score: ")
+    plt.show()
     print "\trandom_hillclimber; score: ", score_schedule, "elapsed time: ", int(elapsed_time / 60),"Min", int(elapsed_time % 60), "sec"
     print update_times
     print score_increase
