e763103b9e4525731cfbd1feccf3379b8edff942
Levyo <Chris_de_Rijcke@hotmail.com>
2016-12-11 15:52:16 +0100

    random schedules, directed roomslot finders

--- ptp.py
+++ ptp.py
@@ -6,25 +6,71 @@ import activity
 import copy
 import matplotlib.pyplot as plt
 import pylab
+import load as loader
 from collections import deque
+
+def scores_random_schedules(schedule, courses, number_random_schedules):
+    random_schedules_scores = []
+    for i in range(number_random_schedules):
+        random_schedule = copy.deepcopy(schedule)
+        random_schedule = loader.random_fill_schedule(random_schedule, courses)
+        random_schedule_score = score.calculate(random_schedule, courses)
+        random_schedules_scores.append(random_schedule_score)
+    srs = random_schedules_scores
+    plt.plot(range(0,len(srs)),srs , "o")
+    srs_average = sum(srs)/len(srs)
+    srs_max = max(srs)
+    plt.title(str(number_random_schedules) + " random schedules scores with an average score of " + str(srs_average) + " and a maximum value of " + str(srs_max))
+    plt.show()
+    return srs
+
 
 def alg(students, courses, rooms, schedule):
 
 
     return schedule
 
-# TODO: Add find empty for specific day/room/time (?)
-# Does find empty work probably? None is returned when a schedule in a algorithm is placed.
-def find_empty(schedule, max_size = 0, day = None):
+def find_empty(schedule, max_size = None, day = None):
     for i, roomslot in enumerate(schedule):
         if day != None:
             if roomslot.day == day:
+                if max_size != None:
+                    if roomslot.room.capacity <= max_size:
+                        if roomslot.activity is None:
+                            return i
+                elif roomslot.activity is None:
+                    return i
+        elif max_size != None:
+            if roomslot.room.capacity <= max_size:
                 if roomslot.activity is None:
                     return i
         elif roomslot.activity is None:
             return i
     return None
 
+# use this random empty when a schedule is being filled
+def find_random_filler(schedule, max_size = None, day = None):
+    search_count = 0
+    while search_count < 100:
+        random_index = random.randrange(len(schedule))
+        if day != None:
+            if schedule[random_index].day == day:
+                if max_size != None:
+                    if schedule[random_index].room.capacity <= max_size:
+                        if schedule[random_index].activity is None:
+                            return random_index
+                elif schedule[random_index].activity is None:
+                    return random_index
+        elif max_size != None:
+            if schedule[random_index].room.capacity <= max_size:
+                if schedule[random_index].activity is None:
+                    return random_index
+        elif schedule[random_index].activity is None:
+            return random_index
+        search_count += 1
+    return find_empty(schedule)
+
+# use this random empty when a filled schedule is being searched
 # maybe a store function that it remembers what has already been added if the same schedule is checked again
 def find_empty_random(schedule, max_size = 0):
     empty_room_list = []
@@ -47,8 +93,8 @@ def random_hillclimber(schedule, courses, desired_score):
     update_times = []
     update_times.append(start_time - start_time)
 
-    for i in range(1):
-    #while score_schedule < desired_score:
+    #for i in range(1):
+    while score_schedule < desired_score:
         new_schedule = copy.deepcopy(schedule)
         old_score = score.calculate(new_schedule, courses)
 
