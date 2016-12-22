28577b118f2d6d03ee46973d0e9758b904d98c98
CarlijnQ <carlijnq@users.noreply.github.com>
2016-12-22 19:57:30 +0100

    Update activity.py

--- activity.py
+++ activity.py
@@ -1,8 +1,10 @@
-class Activity(object):
-    #
-    #   TODO: Add Group and Students
-    #
+"""
 
+Object representing an activity.
+
+"""
+
+class Activity(object):   
     def __init__(self, course, type):
         self.course = course
         self.type = type
