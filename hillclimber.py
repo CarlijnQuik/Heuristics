a11f2cb7591e3d77957763d5441cc7d37b5212a1
Levyo <Chris_de_Rijcke@hotmail.com>
2016-12-22 13:46:03 +0100

    guided_hillclimber updated

--- hillclimber.py
+++ hillclimber.py
@@ -16,7 +16,7 @@ import ptp
     Hillclimber algorithm based on random mutations.
 
 """
-def random_hillclimber(schedule, courses, desired_score, maximum_duration):
+def random_hillclimber(schedule, courses, desired_score, maximum_duration = None):
     # clock the start time of the alghorithm
     start_time = time.time()
     # calculate starting score of schedule
@@ -49,13 +49,16 @@ def random_hillclimber(schedule, courses, desired_score, maximum_duration):
 
         # Calculate score mutated schedule.
         new_score = score.calculate(new_schedule, courses)
-        # Compare score before and after mutation. If new score equals or is higher
-        # than the old score, the mutation is accepted.
-        if new_score >= old_score:
+        # Compare score before and after mutation. The mutation is accepted,
+        # if new score equals or is higher than the old score.
+        if new_score > old_score:
             schedule = copy.deepcopy(new_schedule)
             score_schedule = new_score
+            print score_schedule
 
+        # calculate the in between time
         update_time = time.time() - start_time
+        # if the time is a new second, make timestamp
         if int(update_time) != int(update_times[-1]):
             update_times.append(update_time)
             score_increase.append(score_schedule)
@@ -63,18 +66,24 @@ def random_hillclimber(schedule, courses, desired_score, maximum_duration):
                 if (score_increase[-1] - score_increase[-61]) < 3:
                     break
             # Maximum duration of the algorithm.
-            if (int(update_time)/ 60) >= maximum_duration:
-                break
+            if maximum_duration:
+                if (int(update_time)/ 60) >= maximum_duration:
+                    break
+
+    # calculate how long the alghorithm has been running
     elapsed_time = time.time() - start_time
+
+    # plot of the timestamp and their score
     plt.plot(update_times, score_increase)
     print "random_hillclimber_"+str(desired_score)+"_"+str(int(elapsed_time))
     plt.title("random_hillclimber; desired_score: " +str(desired_score) + ", elapsed_time: "+ str(int(elapsed_time)) + "sec")
     plt.savefig("random_hillclimber; desired_score: ")
     plt.show()
     print "\trandom_hillclimber; score: ", score_schedule, "elapsed time: ", int(elapsed_time / 60),"Min", int(elapsed_time % 60), "sec"
-    print update_times
-    print score_increase
+
+    # return schedule, score and elapsed time
     return {"schedule" : schedule, "score" : score_schedule, "elapsed_time" : elapsed_time}
+
 
 """
 
@@ -114,12 +123,13 @@ def guided_hillclimber(schedule, courses, desired_score):
         day_duplicate = score.check_day_duplicate(new_schedule)
         small_room = score.check_small_room(new_schedule)
         # sort the sub_scores
-        sub_scores_sorted = [special_time_slot[1], multiple_activities[1], day_duplicate[1]]
+        sub_scores_sorted = [special_time_slot[1], multiple_activities[1], day_duplicate[1], small_room[1]]
         sub_scores_sorted.sort(reverse=True)
         # put the scores into a library
         sub_scores["special_time_slot"] = special_time_slot[1]
         sub_scores["multiple_activities"] = multiple_activities[1]
         sub_scores["day_duplicate"] = day_duplicate[1]
+        sub_scores["small_room"] = small_room[1]
         # determine which sub_score belongs to the given score
 
         if rank_in_subscores < len(sub_scores):
@@ -130,34 +140,39 @@ def guided_hillclimber(schedule, courses, desired_score):
             max_sub_score = "random"
 
         #TODO: REMOVE. ONLY FOR TESTING PURPOSES
-        print "max sub score:    ", max_sub_score,
-        print "special_time_slot: ", special_time_slot[1], "multiple_activities: ", multiple_activities[1], "day_duplicate: ", day_duplicate[1]
-        #print "this is day_duplicate score: ", day_duplicate[1], random.choice(day_duplicate[0][random.choice(day_duplicate[0].keys())][1:])
+        # print "max sub score:\t", max_sub_score,
+        # print "special_time_slot: ", special_time_slot[1], "multiple_activities: ", multiple_activities[1], "day_duplicate: ", day_duplicate[1], "small_room: ", small_room[1]
+        # print "this is day_duplicate score: ", day_duplicate[1], random.choice(day_duplicate[0][random.choice(day_duplicate[0].keys())][1:])
 
         # for day_duplicate: check_day_duplicate
         if max_sub_score == "day_duplicate":
             swap_index_1 = random.choice(day_duplicate[0])
-            print new_schedule[swap_index_1].day
+            # print new_schedule[swap_index_1].day
             swap_index_2 = random.randrange(len(schedule))
             while new_schedule[swap_index_1].day == new_schedule[swap_index_2].day:
                 swap_index_2 = random.randrange(len(schedule))
         # for multiple_activities: check_multiple_activities
         elif max_sub_score == "multiple_activities":
-            print "retun of the multiple_activities:  " , random.choice(multiple_activities[0])
-            swap_index_1 = multiple_activities[0][random.choice(multiple_activities[0])]["course1"]
+
+            swap_index_1 = random.choice(multiple_activities[0])
             swap_index_2 = random.randrange(len(schedule))
             while swap_index_2 == swap_index_1:
                 swap_index_2 = random.randrange(len(schedule))
         # for special_time_slot: special_timeslot
         elif max_sub_score == "special_time_slot":
             swap_index_1 = special_time_slot[0][0]
-            print special_time_slot[0][0]
-            print new_schedule[special_time_slot[0][0]].activity.course.seminar_max_students
+            # print special_time_slot[0][0]
+            # print new_schedule[special_time_slot[0][0]].activity.course.seminar_max_students
             empty_place = ptp.find_empty_random(new_schedule)
-            print "the empty_place:", empty_place
+            # print "the empty_place:", empty_place
             swap_index_2 = empty_place
             swap_index_2 = random.randrange(len(schedule))
-         # create random swap indexes
+        elif max_sub_score == "small_room":
+            swap_index_1 = random.choice(small_room[0])
+            swap_index_2 = random.randrange(len(schedule))
+            while schedule[swap_index_2].room.capacity < len(schedule[swap_index_1].activity.students):
+                swap_index_2 = random.randrange(len(schedule))
+        # create random swap indexes
         else:
             swap_index_1 = random.randrange(len(schedule))
             swap_index_2 = random.randrange(len(schedule))
@@ -172,9 +187,10 @@ def guided_hillclimber(schedule, courses, desired_score):
         # increase rank_in_subscores value to prefent getting stuck in local maximum
         rank_in_subscores += 1/20
         # if the new score is higher, accept new schedule
-        if new_score >= old_score:
+        if new_score > old_score:
             schedule = copy.deepcopy(new_schedule)
             score_schedule = new_score
+            print score_schedule
             # reset rank_in_subscorescore
             rank_in_subscores = 0
 
@@ -192,14 +208,14 @@ def guided_hillclimber(schedule, courses, desired_score):
 
     print "\tguided_hillclimber; score: ", score_schedule, "elapsed time: ", int(elapsed_time / 60),"Min", int(elapsed_time % 60), "sec"
     #TODO: REMOVE. print time list
-    print update_times
+    # print update_times
     #TODO: REMOVE. print
-    print score_increase
+    # print score_increase
     # create graph of time and scores
     plt.plot(update_times, score_increase)
     # print plot information for save purposes
     print "guided_hillclimber_"+str(desired_score)+"_"+str(int(elapsed_time/60))+" Min"
-    plt.title("guided_hillclimber; desired_score: " +str(desired_score) + ", elapsed_time: "+ str(int(elapsed_time))+" sec")
+    plt.title("guided_hillclimber; final_score: " + str(score_schedule) + ", elapsed_time: "+ str(int(elapsed_time))+" sec" + "desired_score: " +str(desired_score))
     # show plot
     plt.show()
     return {"schedule" : schedule, "score" : score_schedule, "elapsed_time" : elapsed_time}
