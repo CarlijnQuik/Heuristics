18ce4bc96fd6018c4d13d1880b4bdea99350a579
CarlijnQ <carlijnq@users.noreply.github.com>
2016-12-22 19:26:01 +0100

    Update simulated_annealer.py

--- simulated_annealer.py
+++ simulated_annealer.py
@@ -15,14 +15,16 @@ def decision(probability):
 
 def random_simulated_annealer(schedule, courses, desired_score, initial_temp, maximum_duration):
     start_time = time.time()
-    # calculate starting score of schedule
+    
+    # Calculate starting score of schedule.
     score_schedule = score.calculate(schedule, courses)
     new_schedule = copy.copy(schedule)
     score_increase = []
     score_increase.append(score_schedule)
     update_times = []
     update_times.append(start_time - start_time)
-    # denotes the annealing parameter
+    
+    # Denotes the annealing parameter.
     k = 0
 
     while score_schedule < desired_score:
@@ -30,15 +32,16 @@ def random_simulated_annealer(schedule, courses, desired_score, initial_temp, ma
         new_schedule = copy.deepcopy(schedule)
         old_score = score.calculate(new_schedule, courses)
 
-        # create random swap indexes
+        # Create random swap indexes.
         swap_index_1 = random.randrange(len(schedule))
         swap_index_2 = random.randrange(len(schedule))
 
         for i in range(1):
-            # create random swap indexes
+            # Create random swap indexes.
             swap_index_1 = random.randrange(len(new_schedule))
             swap_index_2 = random.randrange(len(new_schedule))
-            # swap courses and course types
+            
+            # Swap courses and course types.
             temp_activity = new_schedule[swap_index_1].activity
             new_schedule[swap_index_1].activity = new_schedule[swap_index_2].activity
             new_schedule[swap_index_2].activity = temp_activity
@@ -61,7 +64,8 @@ def random_simulated_annealer(schedule, courses, desired_score, initial_temp, ma
             if len(score_increase) >= 61:
                 if (score_increase[-1] - score_increase[-61]) < 3:
                     break
-            # maximum duration of the algorithm
+                    
+            # Maximum duration of the algorithm.
             if (int(update_time)/ 60) >= maximum_duration:
                 break
     elapsed_time = time.time() - start_time
@@ -75,40 +79,47 @@ def random_simulated_annealer(schedule, courses, desired_score, initial_temp, ma
 
 def guided_simulated_annealer(schedule, courses, desired_score, initial_temp, maximum_duration):
     start_time = time.time()
-    # calculate starting score of schedule
+    
+    # Calculate starting score of schedule.
     score_schedule = score.calculate(schedule, courses)
     new_schedule = copy.copy(schedule)
     score_increase = []
     score_increase.append(score_schedule)
     update_times = []
     update_times.append(start_time - start_time)
-    # denotes the annealing parameter
+    
+    # Denotes the annealing parameter.
     k = 0
 
     while score_schedule < desired_score:
-        # calculate current temperature
+        # Calculate current temperature.
         temp = initial_temp * math.pow(0.95, k)
 
-        # create dictionary to determine the order of the scores and valies
+        # Create dictionary to determine the order of the scores and values.
         sub_scores = {}
         rank_in_subscores = 0
-        # create new_schedule
+        
+        # Create new_schedule.
         new_schedule = copy.deepcopy(schedule)
-        # determine score of the former schedule
+        
+        # Determine score of the former schedule.
         old_score = score.calculate(new_schedule, courses)
-        # calculate subscores and determine which subscore is the highest
+        
+        # Calculate subscores and determine which subscore is the highest.
         STS = score.check_special_timeslot(new_schedule)
         CMA = score.check_multiple_activities(new_schedule)
         CDD = score.check_day_duplicate(new_schedule)
-        # sort the sub_scores
+        
+        # Sort the sub_scores.
         sub_scores_sorted = [STS[1], CMA[1], CDD[1]]
         sub_scores_sorted.sort(reverse=True)
-        # put the scores into a library
+        
+        # Put the scores into a library.
         sub_scores["STS"] = STS[1]
         sub_scores["CMA"] = CMA[1]
         sub_scores["CDD"] = CDD[1]
-        # determine which sub_score belongs to the given score
-
+        
+        # Determine which sub_score belongs to the given score.
         if rank_in_subscores < len(sub_scores):
             for sub_score in sub_scores:
                 if sub_scores[sub_score] == sub_scores_sorted[int(rank_in_subscores)]:
@@ -116,25 +127,22 @@ def guided_simulated_annealer(schedule, courses, desired_score, initial_temp, ma
         else:
             max_sub_score = "random"
 
-        #TODO: REMOVE. ONLY FOR TESTING PURPOSES
-        print "max sub score:    ", max_sub_score,
-        print "STS: ", STS[1], "CMA: ", CMA[1], "CDD: ", CDD[1]
-        #print "this is CDD score: ", CDD[1], random.choice(CDD[0][random.choice(CDD[0].keys())][1:])
-
-        # for CDD: check_day_duplicate
+        # For CDD: check_day_duplicate.
         if max_sub_score == "CDD":
             swap_index_1 = random.choice(CDD[0][random.choice(CDD[0].keys())][1:])
             print new_schedule[swap_index_1].day
             swap_index_2 = random.randrange(len(schedule))
             while new_schedule[swap_index_1].day == new_schedule[swap_index_2].day:
                 swap_index_2 = random.randrange(len(schedule))
-        # for CMA: check_multiple_activities
+                
+        # For CMA: check_multiple_activities.
         elif max_sub_score == "CMA":
             swap_index_1 = CMA[0][random.choice(CMA[0].keys())]["course1"]
             swap_index_2 = random.randrange(len(schedule))
             while swap_index_2 == swap_index_1:
                 swap_index_2 = random.randrange(len(schedule))
-        # for STS: special_timeslot
+                
+        # For STS: special_timeslot.
         elif max_sub_score == "STS":
             swap_index_1 = STS[0][0]
             print STS[0][0]
@@ -143,17 +151,18 @@ def guided_simulated_annealer(schedule, courses, desired_score, initial_temp, ma
             print "the empty_place:", empty_place
             swap_index_2 = empty_place
             swap_index_2 = random.randrange(len(schedule))
-         # create random swap indexes
+            
+        # Create random swap indexes.
         else:
             swap_index_1 = random.randrange(len(schedule))
             swap_index_2 = random.randrange(len(schedule))
 
-        # swap courses and course types
+        # Swap courses and course types.
         temp_activity = new_schedule[swap_index_1].activity
         new_schedule[swap_index_1].activity = new_schedule[swap_index_2].activity
         new_schedule[swap_index_2].activity = temp_activity
 
-        # calculate score of new schedule
+        # Calculate score of new schedule.
         new_score = score.calculate(new_schedule, courses)
         # increase rank_in_subscores value to prefent getting stuck in local maximum
         rank_in_subscores += 1/20
